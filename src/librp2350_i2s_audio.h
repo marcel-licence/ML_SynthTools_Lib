f1c6b927ef633d262dee20e6ba0a69804a8f1ba9
Marcel Licence <marcel.licence@gmx.de>
2024-10-29 21:51:34 +0100

    Add i2s support for rp2040

--- src/librp2350_i2s_audio.h
+++ src/librp2350_i2s_audio.h
@@ -1,20 +1,20 @@
 /*
  * ML SynthTools Lib - A modular synthesizer library including organ, electric piano,
  * oscillators, and sound synthesis tools.
- * 
+ *
  * Copyright (C) 2024 Marcel Licence
- * 
+ *
  * This file is part of the ML SynthTools Lib library and is licensed under the ML SynthTools Lib License.
- * 
+ *
  * You may use this file in non-commercial projects, provided that this header
  * and all authorship attributions remain intact.
- * 
+ *
  * For commercial use, please contact marcel.licence.o@gmail.com for licensing terms.
- * 
+ *
  * The source code of this library is not included. Reverse-engineering,
  * decompiling, or any attempt to access the source code from the compiled files
  * is strictly prohibited.
- * 
+ *
  * ML SynthTools Lib is provided "as is," without warranty of any kind. See the LICENSE
  * file for the full license text.
  */
@@ -30,6 +30,13 @@
  * BCK->BCK: 27
  * WCK->LCK: 28
  */
+
+
+#if (defined ARDUINO_RASPBERRY_PI_PICO) || (defined ARDUINO_GENERIC_RP2040)
+#define rp2350_i2s_init(...)    rp2040_i2s_init(__VA_ARGS__)
+#define rp2350_i2s_write_stereo_samples_buff(...)   rp2040_i2s_write_stereo_samples_buff(__VA_ARGS__)
+#endif
+
 
 void rp2350_i2s_init(int data_pin, int clock_base_pin);
 void rp2350_i2s_write_stereo_samples_buff(const float *in_l, const float *in_r, uint32_t sample_count);
